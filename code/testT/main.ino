47a24dba8242a44dc54e3edfe2f20d33bf3fe630
tom <tom@ttlinux.ttlinux.net>
2022-05-22 22:33:34 +0200

    code modifier

---
+++ code/testT/main.ino
@@ -0,0 +1,121 @@
+//Libraries
+// Communication serie on utilise :
+// minicom -b 9600 -o -D /dev/ttyUC0
+
+
+#include <SPI.h>
+#include <PubSubClient.h>
+#include <Ethernet.h>
+#include <EthernetUdp.h>
+#include <Adafruit_Si7021.h>
+#define PORT_MQTT 1883
+#define PORT_UDP 8888
+
+
+void mqttSendHumTmp(int nbVirgule);
+void callback(char* topic, byte* payload, unsigned int length);
+int beginEthSen();
+
+Adafruit_Si7021 sensor = Adafruit_Si7021();
+String request ;
+unsigned long refreshCounter  = 0;
+byte mac [6] = {0x54, 0x34, 0x41, 0x30, 0x30, 0x31};
+IPAddress ipMqtt(194, 199, 227, 239) ;
+IPAddress ipUdp(194, 199, 227, 239) ;
+IPAddress ipDevice(192, 168, 1, 179) ;
+EthernetUDP Udp;
+EthernetClient ethclient; 
+PubSubClient client(ipMqtt,PORT_MQTT,callback,ethclient); 
+
+void setup() 
+{
+	Serial.begin(9600);
+	if(!beginEthSen()){
+		Serial.println("Erreur");
+	}
+	client.connect("arduino");
+}
+void loop() 
+{
+ 	mqttSendHumTmp(2);
+	delay(1000);
+}
+
+
+void udpSendVibration(int nbEchantillon, int nbVirgule, int periodeEchantillonageMs)
+{
+	// Afin d'avoire le nombre de caractere je fait le nombre de chiffre apres la virgule plus deux
+	// qui represente mon premier digits et ma virgule.
+	int nbChar = nbVirgule+2;
+	char tmp[nbEchantillon][nbChar];
+	char data2[nbEchantillon*nbChar];
+	double centsValeurs[nbEchantillon];
+	int i,y;
+	for(i=0;i<nbEchantillon;i++){
+		centsValeurs[i]=analogRead(piezo);
+		centsValeurs[i]=(centsValeurs[i]*5)/1023;
+		delay(periodeEnchantillonageMs);
+	}
+	delay(2000);
+	for(i=0;i<nbEchantillon;i++){
+		dtostrf(centsValeurs[i],nbChar,nbVirgule,tmp[i]);
+		for(y=0;y<nbChar;y++){
+			data2[(nbChar*i)+y]=tmp[i][y];
+			Serial.println(data2[(i*nbChar)+y]);	
+		}	
+	}
+
+	Udp.beginPacket(ipUdp,PORT_UDP);
+	Udp.write(data2);
+	Udp.endPacket();
+	Serial.println(Ethernet.localIP());
+	Udp.begin(PORT_UDP);
+}
+// Lance tout les fonction. Ehternet, UDP, MQTT et le capteur. 
+int beginEthSen()
+{
+
+	if (!sensor.begin()) 
+	{
+		Serial.println("Did not find Si7021 sensor!");
+		return -1;
+	}
+
+	Serial.println(F("Initialize System"));
+	Ethernet.begin(mac, ipDevice);
+
+	while (!Ethernet.begin(mac)) 
+	{
+		Serial.println(F("failed. Retrying in 1 secondsi."));
+		return -1;
+	}
+
+	pinMode(2, OUTPUT);
+	Serial.print(F("IP Address: "));
+	Serial.println(Ethernet.localIP());
+	return 0;
+}
+
+
+// Cette fonction recolte et envoie l'humiditer et la température en MQTT. nbVirgule permet de spécifier le nombre de valeur apres la virgule.
+void mqttSendHumTmp(int nbVirgule)
+{
+
+	double hum,temp;
+	char Ctemp[2+nbVirgule];
+	char Chum[2+nbVirgule];
+	hum = sensor.readHumidity();
+	temp = sensor.readTemperature();
+	dtostrf(temp,2,nbVirgule,Ctemp);
+	dtostrf(hum,2,nbVirgule,Chum);
+	Serial.println(Ctemp);
+	if(!client.publish("topic",Ctemp))
+	{
+		Serial.println("error");
+	}
+	if(!client.publish("topic",Chum))
+	{
+		Serial.println("error");
+	}
+
+}
